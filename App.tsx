b9b8caa0ed9fae8ed3b6bf9c53869d3c2ec50bca
marco <marcoaureliotomaz@gmail.com>
2024-03-21 02:06:46 -0300

    Ajustes de design

--- App.tsx
+++ App.tsx
@@ -1,118 +1,68 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, { Component } from "react";
+import { View, Text , Button, StyleSheet, Image   } from 'react-native';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+class App extends Component{
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+  chamaLogin(){
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+}
+
+
+  render(): React.ReactNode {
+    return(
+      <View style={styles.container}>
+      <View style={styles.loginContainer}>
+        <Button title="Login" onPress={this.chamaLogin} />
+      </View>
+      <View style={styles.centeredContainer}>
+        <Text style={{fontSize:40, color:'#02026b'}}>PesqApp</Text>
+        <LogoImage></LogoImage>
+      </View>
+      <View style={styles.centeredContainer}>
+        <Button color="#0000FF" title="Pesquisa de opnião  1" onPress={() => console.log('Botão 1 pressionado')} />
+        <View style={styles.space} />
+        <Button color="#2855db" title="Pesquisa de opnião  2" onPress={() => console.log('Botão 2 pressionado')} />
+        <View style={styles.space} />
+        <Button color="#517de6" title="Pesquisa de opnião  3" onPress={() => console.log('Botão 3 pressionado')} />
+      
+      </View>
     </View>
-  );
+    );
+  }
 }
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+export default App;
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+class LogoImage extends Component{
+  render(): React.ReactNode {
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+    let logo = 'https://www.questionpro.com/blog/wp-content/uploads/2018/08/qp-market-research-e1598542900924-450x276.png';
+    return(
+      <Image source={{uri:logo}}
+        style={{width:300, height:300, top:1}}></Image>
+    )
+  }
 }
+
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  loginContainer: {
+    position: 'absolute',
+    top: 20,
+    right: 20,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  centeredContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  highlight: {
-    fontWeight: '700',
+  space: {
+    height: 20, // Altere essa altura conforme necessário para o espaço vertical desejado
   },
 });
 
-export default App;
